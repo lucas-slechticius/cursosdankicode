f3ac50199a849400de36d67ceaea090d7d5bd6f7
Lucas Slechticius <slechticiuslucas@gmail.com>
2023-01-26 17:46:38 -0300

    finalizando testes com API do Google Maps JS

--- Curso Front/Projeto_03/js/function.js
+++ Curso Front/Projeto_03/js/function.js
@@ -13,6 +13,29 @@ window.onload = function(){
         map = new google.maps.Map(document.getElementById("mapa"), mapProp);
     }
 
+    function addMarker(lat, long, icon, content){
+        var latLng = {'lat':lat,'lng':long};
+        var marker = new google.maps.Marker({
+            position:latLng,
+            map:map,
+            icon:icon
+        });
+
+        var infoWindow = new google.maps.InfoWindow({
+            content:content,
+            maxWidth:200,
+            pixelOffset: new google.maps.Size(0,20)
+        })
+
+        infoWindow.open(map,marker);
+    }
+
     initialize();
 
+    addMarker(-27.616637,-48.592328,'','');
+
+    setTimeout(function(){
+        map.panTo({'lat':-23.550520,'lng':-46.633309});
+    },2000)
+   
 }
