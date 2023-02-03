32d94218564ce32da446cab6ec6da30f9bb7c30b
Lucas Slechticius <slechticiuslucas@gmail.com>
2023-02-03 18:28:39 -0300

    Mais estudos sobre o JQuery

--- Curso Front/jQuery/js/main.js
+++ Curso Front/jQuery/js/main.js
@@ -1,3 +1,15 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
 alert("ola mundo")
 })
+
+$(window).on('load',function(){
+    alert("olá mundo")
+})*/
+
+$(function(){
+
+    $('body').click(function(){
+        $('.box').css('background-color','green');
+    })
+
+});
